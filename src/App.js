f95695903db00c5dc45befbe934d39781cc7fa3f
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-07 18:14:59 +0545

    change app to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
