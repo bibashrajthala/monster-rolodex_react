f27e4ecb6034663aeaac33f585357d4bf3248842
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-07 18:41:38 +0545

    mapping arrays to elements

--- src/App.js
+++ src/App.js
@@ -5,34 +5,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Bibash", lastName: "Rajthala" },
-      company: "ZTM",
+      monsters: [{ name: "Frank" }, { name: "Jacky" }, { name: "Andrei" }],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <h1>
-          hi i am {this.state.name.firstName} {this.state.name.lastName}
-        </h1>
-        <p>i work in {this.state.company}</p>
-        <button
-          onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  name: { firstName: "Ram", lastName: "Katwal" },
-                };
-              },
-              () => {
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          Change name
-        </button>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
