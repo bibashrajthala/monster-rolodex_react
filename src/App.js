bfc0d51d5eefa5eca31ad06fc7f544d79a4d3151
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-08 18:14:38 +0545

    props in functional component,convert cardlist to functional

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import SearchBox from "./components/search-box/search-box.component";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
-  const [searchString, setSearchString] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
-  console.log("render");
-  // console.log(monsters);
 
   useEffect(() => {
-    console.log("effect1");
+    // console.log("effect1");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
@@ -22,20 +19,19 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+    // console.log("effect2");
+
     const newfilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchField);
     });
     setFilteredMonsters(newfilteredMonsters);
   }, [monsters, searchField]);
-  console.log(filteredMonsters);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
-  const onStringChange = (event) => {
-    setSearchString(event.target.value);
-  };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
@@ -44,7 +40,6 @@ const App = () => {
         placeholder="Search Monsters"
         onChangeHandler={onSearchChange}
       />
-      <SearchBox placeholder="Set string" onChangeHandler={onStringChange} />
       <CardList monsters={filteredMonsters} />
     </div>
   );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,17 +3,17 @@ import Card from "../card/card.component";
 
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  //   or
+  // const CardList = (props) => {
+  //  const {monsters} = props;
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
