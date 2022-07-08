0a7426e370960f209e6998680bc62ecad4006044
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-07-08 18:23:26 +0545

    finishing touch,convert all other components to functional

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,23 +1,17 @@
-import { Component } from "react";
-
 import "./card.styles.css";
 
-class Card extends Component {
-  render() {
-    const { monster } = this.props;
-
-    const { id, name, email } = monster;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          src={`https://robohash.org/${id}?set=set2`}
-          alt={`monster ${name}`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2`}
+        alt={`monster ${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
